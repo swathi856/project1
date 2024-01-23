b3c4d9d87351f9bc237061290bf348d6a478cfcb
swathi856 <chswathi856@gmail.com>
2024-01-23 12:57:03 +0530

    first commit

---
+++ And.java
@@ -0,0 +1,7 @@
+class jm
+{
+ public static void main(String args[])
+  {
+   System.out.println(true&&false);
+   }
+}
---
+++ hi.java
@@ -0,0 +1,19 @@
+class j
+{
+  public static void main(String args[])
+  {
+    int x=5;
+    int y=7;
+    int z;
+    System.out.println("after swaping");
+    z=x;//z=5
+    x=y;//x=7
+    y=z;//y=5
+    System.out.println(x);
+    System.out.println(y);
+    
+} 
+}
+    
+    
+    
